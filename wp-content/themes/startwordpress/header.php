76ce9404b31a2ac7b7892e4de6ee97458338c220
Chien <chien@autoketing.com>
2019-08-24 15:09:53 +0700

    Add navbar

--- wp-content/themes/startwordpress/header.php
+++ wp-content/themes/startwordpress/header.php
@@ -16,7 +16,7 @@
 <body>
 <div class="container-fluid">
     <div class="container">
-        <nav class="navbar navbar-expand-lg pl-0 pr-0">
+        <nav class="navbar navbar-expand-lg navbar-light pl-0 pr-0">
             <a class="navbar-brand" href="#">Laargo</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
